dd68a4196ff80c0c4b335254b4fdd789c541349a
jotdhessi <harmanjot28082004@gmail.com>
2023-12-07 16:22:19 -0500

    version5

--- Program.py
+++ Program.py
@@ -5,18 +5,23 @@ i=1
 while x!=6:
    
     if x==1:
-
-        a=input(" What is the product-name and brand ")
-        b=float(input(" What is price of the product "))
-        Shoppingcart.update({a:b})
-        print(Shoppingcart)
+        if len(Shoppingcart)>=5:
+                     print("The cart is full")
+        else:
+         a=input(" What is the product-name and brand ")
+         b=float(input(" What is price of the product "))
+         Shoppingcart.update({a:b})
+        
+  
+        
     elif x==2:
 
         c=(input("What you want to search "))
         if c in Shoppingcart:
             print(c,"-",Shoppingcart[c])
+            print(Shoppingcart)
         else:
-            print("Not Found")         
+            print("No Product Exists With This name")         
 
     elif x==3:
          d=(input("What product do you want to delete "))
@@ -24,13 +29,16 @@ while x!=6:
              del Shoppingcart[d]
              print("Delete Product Successfully")
          else :
-             print("Not found")   
+             print("Product not found in cart")   
     elif x==4:
+       
          print(Shoppingcart)
     elif x==5:
+          g=input("Complete purchase(Y/N)")
+          
           print(Shoppingcart)
-
-           
+    
+    
     print("WELCOME TO THE AMANDO SHOPPING SITE \n1.Add a product to the cart \n2.Search for a product\n3.Delete a product from the cart\n4.Display the contents of the cart.\n5.Purchase items\n6.Quit ")
     x = int(input("Enter Value: "))
    
